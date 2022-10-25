879fc02450e95321c09b4bf1363796d6418a10d8
AutoDave91 <davidwnewman91@gmail.com>
2022-10-25 16:11:27 -0500

    character score option progress

--- src/pages/CharacterBuild.js
+++ src/pages/CharacterBuild.js
@@ -11,12 +11,12 @@ const { playableRaces, attributes, defects } = defaultData
 const CharacterBuild = () => {
     const [usedPoints, setUsedPoints] = useState(0);
     const [selectedRace, setSelectedRace] = useState({ 'cost': 0 });
-    const [strength, setStrength] = useState({ 'value': 0 })
-    const [dexterity, setDexterity] = useState({ 'value': 0 })
-    const [constitution, setConstitution] = useState({ 'value': 0 })
-    const [intelligence, setIntelligence] = useState({ 'value': 0 })
-    const [wisdom, setWisdom] = useState({ 'value': 0 })
-    const [charisma, setCharisma] = useState({ 'value': 0 })
+    const [strength, setStrength] = useState({ 'score': 0, 'value': 0 })
+    const [dexterity, setDexterity] = useState({ 'score': 0, 'value': 0 })
+    const [constitution, setConstitution] = useState({ 'score': 0, 'value': 0 })
+    const [intelligence, setIntelligence] = useState({ 'score': 0, 'value': 0 })
+    const [wisdom, setWisdom] = useState({ 'score': 0, 'value': 0 })
+    const [charisma, setCharisma] = useState({ 'score': 0, 'value': 0 })
     const [overBudget, setOverBudget] = useState(false)
     const [attribute, setAttribute] = useState({ 'value': 0 })
     const [attList, setAttList] = useState([])
@@ -26,11 +26,10 @@ const CharacterBuild = () => {
     const [charClass, setCharClass] = useState('')
     const [abilityScoreMethod, setAbilityScoreMethod] = useState('Player Choice')
     const [abilitySpent, setAbilitySpent] = useState(0)
+    const [availableScores, setAvailableScores] = useState([['8', -1], ['10', 0], ['12', 1], ['13', 1], ['14', 2], ['15', 2]])
 
     const absOptions = [
-        // ['1', -5], ['2', -4], ['3', -4], ['4', -3],
-        ['5', -3], ['6', -2], ['7', -2], ['8', -1], ['9', -1], ['10', 0], ['11', 0], ['12', 1], ['13', 1], ['14', 2], ['15', 2], ['16', 3], ['17', 3], ['18', 4]
-        // , ['19', 4], ['20', 5], ['21', 5], ['22', 6], ['23', 6], ['24', 7], ['25', 7], ['26', 8], ['27', 8], ['28 ', 9], ['29', 9], ['30', 10]
+        ['3', -4], ['4', -3], ['5', -3], ['6', -2], ['7', -2], ['8', -1], ['9', -1], ['10', 0], ['11', 0], ['12', 1], ['13', 1], ['14', 2], ['15', 2], ['16', 3], ['17', 3], ['18', 4]
     ];
 
     const handleRaceChange = (event) => {
@@ -46,6 +45,37 @@ const CharacterBuild = () => {
         if (event.target.value) {
             let abilityScoreArr = event.target.value.split(",");
             let abilityScoreObj = { 'score': abilityScoreArr[0], 'value': +abilityScoreArr[1] }
+
+            if (ability === 'strength') {
+                setStrength(abilityScoreObj)
+            }
+            if (ability === 'dexterity') {
+                setDexterity(abilityScoreObj)
+            }
+            if (ability === 'constitution') {
+                setConstitution(abilityScoreObj)
+            }
+            if (ability === 'intelligence') {
+                setIntelligence(abilityScoreObj)
+            }
+            if (ability === 'wisdom') {
+                setWisdom(abilityScoreObj)
+            }
+            if (ability === 'charisma') {
+                setCharisma(abilityScoreObj)
+            }
+        }
+    };
+    const handleAbilityChoice = (event, ability) => {
+        if (event.target.value) {
+            let newArr = []
+            let abilityScoreArr = event.target.value.split(",");
+            let abilityScoreObj = { 'score': abilityScoreArr[0], 'value': +abilityScoreArr[1] }
+
+            availableScores.forEach((score) => { if (score[0] !== abilityScoreObj.score) { newArr.push(score); console.log(score) } })
+
+            setAvailableScores(newArr)
+            console.log(availableScores)
 
             if (ability === 'strength') {
                 setStrength(abilityScoreObj)
@@ -108,26 +138,20 @@ const CharacterBuild = () => {
     // }
 
     useEffect(() => {
-        console.log(selectedRace)
-        const pointBuy = () => {
-            let total = (
-                +strength.value +
-                +dexterity.value +
-                +constitution.value +
-                +intelligence.value +
-                +wisdom.value +
-                +charisma.value
-            )
+        console.log(selectedRace);
 
-            setAbilitySpent(total)
-
-        }
         const totalPoints = () => {
             let attTotal = 0;
             let defectTotal = 0;
             attList.forEach((att) => attTotal += +att.value)
             defectList.forEach((defect) => defectTotal -= +defect.value)
             let total = (
+                +strength.score +
+                +dexterity.score +
+                +constitution.score +
+                +intelligence.score +
+                +wisdom.score +
+                +charisma.score +
                 +selectedRace.cost +
                 +attTotal +
                 +defectTotal
@@ -141,8 +165,7 @@ const CharacterBuild = () => {
         }
 
         totalPoints();
-        pointBuy();
-    }, [setOverBudget, selectedRace.cost, usedPoints, attribute.value, defect.value, strength.value, dexterity.value, constitution.value, intelligence.value, wisdom.value, charisma.value])
+    }, [setOverBudget, selectedRace.cost, usedPoints, attribute.score, defect.score, strength.score, dexterity.score, constitution.score, intelligence.score, wisdom.score, charisma.score])
 
     return (
         <div>
@@ -163,25 +186,78 @@ const CharacterBuild = () => {
                         <h3>Ability Scores</h3>
                         <div>
                             <button onClick={(e) => { e.preventDefault(); setAbilityScoreMethod('Player Choice') }}>Player Choice</button>
-                            <button onClick={(e) => { e.preventDefault(); setAbilityScoreMethod('Point Buy') }}>Point Buy</button>
+                            <button onClick={(e) => { e.preventDefault(); setAbilityScoreMethod('Fixed Numbers') }}>Fixed Numbers</button>
                             <button onClick={(e) => { e.preventDefault(); setAbilityScoreMethod('Roll Dice') }}>Roll Dice</button>
                         </div>
+                        {abilityScoreMethod === 'Fixed Numbers' && (
+                            <div>
+                                <p>{abilityScoreMethod}</p>
+                                <div className="abilityScoreWrapper">
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Strength</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'strength')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+                                        </select>
+                                        <div>{strength && <p>{strength.value >= 1 ? `+${strength.value}` : strength.value}</p>}</div>
+                                    </div>
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Dexterity</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'dexterity')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+
+                                        </select>
+                                        <div>{dexterity && <p>{dexterity.value >= 1 ? `+${dexterity.value}` : dexterity.value}</p>}</div>
+                                    </div>
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Constitution</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'constitution')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+
+                                        </select>
+                                        <div>{constitution && <p>{constitution.value >= 1 ? `+${constitution.value}` : constitution.value}</p>}</div>
+                                    </div>
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Intelligence</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'intelligence')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+
+                                        </select>
+                                        <div>{intelligence && <p>{intelligence.value >= 1 ? `+${intelligence.value}` : intelligence.value}</p>}</div>
+                                    </div>
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Wisdom</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'wisdom')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+
+                                        </select>
+                                        <div>{wisdom && <p>{wisdom.value >= 1 ? `+${wisdom.value}` : wisdom.value}</p>}</div>
+                                    </div>
+                                    <div className='abilityScoreBlock'>
+                                        <h4>Charisma</h4>
+                                        <select onChange={(e) => handleAbilityChoice(e, 'charisma')}>
+                                            <option value={['', 0]} >Ability Score</option>
+                                            {availableScores.map((abs) => <option value={abs} >{abs[0]}</option>)}
+
+                                        </select>
+                                        <div>{charisma && <p>{charisma.value >= 1 ? `+${charisma.value}` : charisma.value}</p>}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                         {abilityScoreMethod === 'Player Choice' && (
                             <div>
                                 <p>{abilityScoreMethod}</p>
-                            </div>
-                        )}
-                        {abilityScoreMethod === 'Point Buy' && (
-                            <div>
-                                <p>{abilityScoreMethod}</p>
-                                <p>Point Buy: {`${abilitySpent}/__`}</p>
                                 <div className="abilityScoreWrapper">
                                     <div className='abilityScoreBlock'>
                                         <h4>Strength</h4>
                                         <select onChange={(e) => handleAbilityScoreChange(e, 'strength')}>
                                             <option value={['', 0]} >Ability Score</option>
                                             {absOptions.map((abs) => <option value={abs} >{abs[0]}</option>)}
-
                                         </select>
                                         <div>{strength && <p>{strength.value >= 1 ? `+${strength.value}` : strength.value}</p>}</div>
                                     </div>
@@ -236,6 +312,11 @@ const CharacterBuild = () => {
                         {abilityScoreMethod === 'Roll Dice' && (
                             <div>
                                 <p>{abilityScoreMethod}</p>
+                                <div>
+                                    <button>Randomly with 4d6</button>
+                                    <button>Randomly with Excess 3d6</button>
+                                    <button>Strict 3d6 in Order</button>
+                                </div>
                             </div>
                         )}
                         <div className="abilityScoreWrapper">
@@ -263,44 +344,46 @@ const CharacterBuild = () => {
                         </div>
                     </div>
                 </form>
-                <div>
-                    <h2>Character Preview</h2>
-                    <h3>{charName && charName}</h3>
-                    <h4>Points Remaining: {80 - usedPoints}</h4>
-                    <h4>{selectedRace && selectedRace.race} {charClass && charClass}</h4>
-                    <div className="abilityScoreWrapper">
-                        <div className='abilityScoreBlock'>
-                            <h4>Strength</h4>
-                            <p>{strength && strength.score}</p>
-                            {<p>{strength.value >= 1 ? `+${strength.value}` : strength.value}</p>}
-                        </div>
-                        <div className='abilityScoreBlock'>
-                            <h4>Dexterity</h4>
-                            <p>{dexterity && dexterity.score}</p>
-                            {<p>{dexterity.value >= 1 ? `+${dexterity.value}` : dexterity.value}</p>}
-                        </div>
-                        <div className='abilityScoreBlock'>
-                            <h4>Constitution</h4>
-                            <p>{constitution && constitution.score}</p>
-                            {<p>{constitution.value >= 1 ? `+${constitution.value}` : constitution.value}</p>}
-                        </div>
-                        <div className='abilityScoreBlock'>
-                            <h4>Intelligence</h4>
-                            <p>{intelligence && intelligence.score}</p>
-                            {<p>{intelligence.value >= 1 ? `+${intelligence.value}` : intelligence.value}</p>}
-                        </div>
-                        <div className='abilityScoreBlock'>
-                            <h4>Wisdom</h4>
-                            <p>{wisdom && wisdom.score}</p>
-                            {<p>{wisdom.value >= 1 ? `+${wisdom.value}` : wisdom.value}</p>}
-                        </div>
-                        <div className='abilityScoreBlock'>
-                            <h4>Charisma</h4>
-                            <p>{charisma && charisma.score}</p>
-                            {<p>{charisma.value >= 1 ? `+${charisma.value}` : charisma.value}</p>}
+                {usedPoints !== 0 &&
+                    <div>
+                        <h2>Character Preview</h2>
+                        <h3>{charName && charName}</h3>
+                        <h4>Points Remaining: {80 - usedPoints}</h4>
+                        <h4>{selectedRace && selectedRace.race} {charClass && charClass}</h4>
+                        <div className="abilityScoreWrapper">
+                            <div className='abilityScoreBlock'>
+                                <h4>Strength</h4>
+                                <p>{strength && strength.score}</p>
+                                {<p>{strength.value >= 1 ? `+${strength.value}` : strength.value}</p>}
+                            </div>
+                            <div className='abilityScoreBlock'>
+                                <h4>Dexterity</h4>
+                                <p>{dexterity && dexterity.score}</p>
+                                {<p>{dexterity.value >= 1 ? `+${dexterity.value}` : dexterity.value}</p>}
+                            </div>
+                            <div className='abilityScoreBlock'>
+                                <h4>Constitution</h4>
+                                <p>{constitution && constitution.score}</p>
+                                {<p>{constitution.value >= 1 ? `+${constitution.value}` : constitution.value}</p>}
+                            </div>
+                            <div className='abilityScoreBlock'>
+                                <h4>Intelligence</h4>
+                                <p>{intelligence && intelligence.score}</p>
+                                {<p>{intelligence.value >= 1 ? `+${intelligence.value}` : intelligence.value}</p>}
+                            </div>
+                            <div className='abilityScoreBlock'>
+                                <h4>Wisdom</h4>
+                                <p>{wisdom && wisdom.score}</p>
+                                {<p>{wisdom.value >= 1 ? `+${wisdom.value}` : wisdom.value}</p>}
+                            </div>
+                            <div className='abilityScoreBlock'>
+                                <h4>Charisma</h4>
+                                <p>{charisma && charisma.score}</p>
+                                {<p>{charisma.value >= 1 ? `+${charisma.value}` : charisma.value}</p>}
+                            </div>
                         </div>
                     </div>
-                </div>
+                }
             </div >
         </div >
     );
